d604e113ac4126c34ec2e3f0885d7d3725b4cff2
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2025-11-28 20:01:45 +0700

    feat: delete web and spec

--- src/main/java/com/sisko/exam/web/dto/ApiError.java
+++
@@ -1,5 +0,0 @@
-package com.sisko.exam.web.dto;
-
-
-
-public record ApiError(String code, String message) {}
--- src/main/java/com/sisko/exam/web/dto/AuthDTOs.java
+++
@@ -1,9 +0,0 @@
-package com.sisko.exam.web.dto;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public class AuthDTOs {
-    public record LoginReq(@Email String email, @NotBlank String password) {}
-    public record LoginResp(String token) {}
-}
